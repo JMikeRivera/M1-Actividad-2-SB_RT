f52ebeae51a7101eeda0b5cd7529e42d5b899189
4ll3ssandro <rodrigovela2006@gmail.com>
2026-04-23 10:35:17 -0600

    TareaB

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Scoreboard   from './components/Scoreboard'
 import EventFeed    from './components/EventFeed'
 import NewEventForm from './components/NewEventForm'
 import MatchChat from './components/MatchChat'   // [E]
+import PresenceIndicator from './components/PresenceIndicator'
 
 export default function App() {
   const [match,  setMatch]  = useState(null)
@@ -117,6 +118,8 @@ export default function App() {
       <h1 style={{ fontSize: '1.1rem', color: '#888', marginBottom: '1rem', textTransform: 'uppercase', letterSpacing: '0.08em' }}>
         Panel de Partido en Vivo
       </h1>
+
+      <PresenceIndicator />    {/* [B] */}
 
       <Scoreboard
         match={match}
